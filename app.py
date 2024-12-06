740959516fb832929fe397597ce9469691682871
JAlejandroQuevedo <alejandro_ya23@hotmail.com>
2024-12-06 09:29:11 -0700

    Commit

--- app.py
+++ app.py
@@ -21,24 +21,16 @@ from config import DefaultConfig
 CONFIG = DefaultConfig()
 
 # Create adapter.
-# See https://aka.ms/about-bot-adapter to learn more about how bots work.
 SETTINGS = BotFrameworkAdapterSettings(CONFIG.APP_ID, CONFIG.APP_PASSWORD)
 ADAPTER = BotFrameworkAdapter(SETTINGS)
 
-
 # Catch-all for errors.
 async def on_error(context: TurnContext, error: Exception):
-    # This check writes out errors to console log .vs. app insights.
-    # NOTE: In production environment, you should consider logging this to Azure
-    #       application insights.
     print(f"\n [on_turn_error] unhandled error: {error}", file=sys.stderr)
     traceback.print_exc()
 
-    # Send a message to the user
     await context.send_activity("The bot encountered an error or bug.")
-    # Send a trace activity if we're talking to the Bot Framework Emulator
     if context.activity.channel_id == "emulator":
-        # Create a trace activity that contains the error object
         trace_activity = Activity(
             label="TurnError",
             name="on_turn_error Trace",
@@ -47,19 +39,15 @@ async def on_error(context: TurnContext, error: Exception):
             value=f"{error}",
             value_type="https://www.botframework.com/schemas/error",
         )
-        # Send a trace activity, which will be displayed in Bot Framework Emulator
         await context.send_activity(trace_activity)
-
 
 ADAPTER.on_turn_error = on_error
 
 # Create the Bot
 BOT = alfabot()
 
-
 # Listen for incoming requests on /api/messages
 async def messages(req: Request) -> Response:
-    # Main bot message handler.
     if "application/json" in req.headers["Content-Type"]:
         body = await req.json()
     else:
@@ -73,21 +61,9 @@ async def messages(req: Request) -> Response:
         return json_response(data=response.body, status=response.status)
     return Response(status=HTTPStatus.OK)
 
-
-# Convert the aiohttp app to ASGI-compatible app
 app = web.Application(middlewares=[aiohttp_error_middleware])
 app.router.add_post("/api/messages", messages)
 
-# This is where we convert aiohttp to ASGI to be compatible with Uvicorn
-from asgi_aiohttp import ASGIApplication
-
-# Convert the aiohttp app to ASGIApp and run it with Uvicorn
-asgi_app = ASGIApplication(app)
-
 if __name__ == "__main__":
-    try:
-        # Replace web.run_app with uvicorn to use ASGI with uvicorn
-        import uvicorn
-        uvicorn.run(asgi_app, host="0.0.0.0", port=3979)
-    except Exception as error:
-        raise error
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=3979)
